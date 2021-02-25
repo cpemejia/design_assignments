d3c475caccc851c04b453b8740900c663af31490
cpemejia <56175869+cpemejia@users.noreply.github.com>
2021-02-24 23:42:47 -0800

    Create DA_2A.c

---
+++ DA_2A/DA_2A.c
@@ -0,0 +1,36 @@
+/*
+ * DA_2A.c
+ *
+ * Created: 2/23/2021 5:44:39 PM
+ * Author : ElmerOMejia
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000UL 
+#include <util/delay.h>
+
+int main (void)
+{
+	DDRB |= (1<<PB3);		// set PB3 output
+	PORTB |= (1<<PB3);      // set PB3 off
+	//set PORTC.3 for input
+	DDRC &= (0 << 3);
+	PORTC |= (1 << 3);
+	
+	while(1){
+		PORTB |= (1<<PB3);  // output PB3 on
+		_delay_ms(750);     // delay of 0.75s
+		PORTB &= ~(1<<PB3); // output PB3 off
+		_delay_ms(250);     // delay of 0.25s
+		
+		if (!(PINC & (1<<PINC3)))    
+		{
+			PORTB &= ~(1<<PB3); // output PB3 off
+			PORTB &= ~(1<<2); // set PORTB.2 for output
+			_delay_ms(1750);  // delay of 2s
+			PORTB |= (1<<2);  // PB2 off
+			PORTB |= (1<<PB3);		// PB3 output on 
+	   }
+	}
+	return 1;
+}
